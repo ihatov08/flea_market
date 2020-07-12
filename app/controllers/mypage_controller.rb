b2c4fa6cfa245d39e3e83032fac2c511a93e23a9
ihatov08 <ihatov08@gmail.com>
2020-07-12 13:30:13 +0900

    ログアウトページの実装

--- app/controllers/mypage_controller.rb
+++ app/controllers/mypage_controller.rb
@@ -1,4 +1,10 @@
 class MypageController < ApplicationController
+  before_action :authenticate_user!
+
   def index
+  end
+
+  def logout
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
   root to: "home#index"
 
   get "mypage" => "mypage#index", as: :mypage
+  get "mypage/logout" => "mypage#logout", as: :logout
 
   get "pages/:title" => "pages#show", as: :page
 end
